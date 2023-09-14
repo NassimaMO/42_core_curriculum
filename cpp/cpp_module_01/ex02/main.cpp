92fc07544f702da86e5f8fbe37d9db320aa8edb6
Nassima <nassimamouslim@gmail.com>
2023-09-14 10:50:33 +0200

    little changes

--- cpp/cpp_module_01/ex02/main.cpp
+++ cpp/cpp_module_01/ex02/main.cpp
@@ -16,14 +16,14 @@
 int main( void )
 {
     std::string brain( "HI THIS IS BRAIN" );
-    std::string *stringPTR(&brain);
-    std::string &stringREF(brain);
+    std::string *stringPTR( &brain );
+    std::string &stringREF( brain );
 
-    std::cout << "string address: " << &brain << std::endl;
-    std::cout << "string address of stringPTR: " << &stringPTR << std::endl;
-    std::cout << "string address of stringREF: " << &stringREF << std::endl;
+    std::cout << "address of brain: " << &brain << std::endl;
+    std::cout << "address of stringPTR: " << &stringPTR << std::endl;
+    std::cout << "address of stringREF: " << &stringREF << std::endl;
     std::cout << std::endl;
-    std::cout << "value: " << brain << std::endl;
+    std::cout << "value of brain: " << brain << std::endl;
     std::cout << "value of stringPTR: " << *stringPTR << std::endl;
     std::cout << "value of stringREF: " << stringREF << std::endl;
 
