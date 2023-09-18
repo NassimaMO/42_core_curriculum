392bd37d2d2b8caf184492642866ad9eaa3db58c
Nassima MOUSLIM <nassimamouslim@gmail.com>
2023-09-18 15:14:32 +0200

    cpp03 began

---
+++ cpp/cpp_module_03/ex00/ClapTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class   ClapTrap
+{
+    public:
+
+    ClapTrap( void );
+    ClapTrap( std::string name );
+    ~ClapTrap();
+
+    void    attack( const std::string& target );
+    void    takeDamage( unsigned int amount );
+    void    beRepaired( unsigned int amount );
+
+    private:
+
+    std::string _name;
+    int         _hit;
+    int         _energy;
+    int         _attack;
+};
+
+#endif
