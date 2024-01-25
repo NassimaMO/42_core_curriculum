17db81753f564006b575eb296f481a9fa9605d1d
Nassima MOUSLIM <nassimamouslim@gmail.com>
2024-01-25 16:46:30 +0100

    cpp08:ex02 done; cpp08 concluded

--- cpp05-09/cpp08/ex02/MutantStack.hpp
+++ cpp05-09/cpp08/ex02/MutantStack.hpp
@@ -2,10 +2,36 @@
 # define MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
 
-class MutantStack : public stack<T, C>
+template< typename T >
+class MutantStack : public std::stack< T >
 {
+    public:
 
-}
+    MutantStack( void ){}
+    MutantStack( const MutantStack &m ){ *this = m; }
+    MutantStack&    operator=( const MutantStack &m )
+    {
+        if ( this != *m )
+        {
+            *this = m;
+        }
+        return ( &this );
+    }
+    ~MutantStack(){}
+ // Container_Type: This parameter is the type of container for which the iterator is declared.
+    typedef typename std::stack< T >::container_type::iterator    iterator;
+
+    iterator begin( void )
+    {
+        return this->c.begin();
+    }
+
+    iterator end( void )
+    {
+        return this->c.end();
+    }
+};
 
 #endif
